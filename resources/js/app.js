73cdb3bb4c2d66371ad76979d025b1d173d243cf
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-03 19:40:01 +0500

    try v1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,19 @@
 import Alpine from 'alpinejs'
+import flatpickr from "flatpickr";
 
 window.Alpine = Alpine
+window.flatpickr = flatpickr
+
+import("./../../node_modules/flatpickr/dist/flatpickr.min.css");
+import "./../../vendor/power-components/livewire-powergrid/dist/powergrid.css";
+import("./../../vendor/power-components/livewire-powergrid/dist/tom-select.css");
+
+import TomSelect from "tom-select";
+window.TomSelect = TomSelect
+
+//import SlimSelect from 'slim-select'
+//window.SlimSelect = SlimSelect
 
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
-import './../../vendor/power-components/livewire-powergrid/dist/powergrid.css'
 
 Alpine.start()
