997a226d029d674beab751280433c05ca56878e7
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-03 19:35:22 +0500

    update again app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import Alpine from 'alpinejs'
-import flatpickr from "flatpickr";
 
 window.Alpine = Alpine
-window.flatpickr = flatpickr
 
-import("./../../node_modules/flatpickr/dist/flatpickr.min.css");
-import("./../../vendor/power-components/livewire-powergrid/dist/powergrid.css")
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid.css'
 
 Alpine.start()
