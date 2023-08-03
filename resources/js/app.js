cca027a72042cc83399efe40f06d3d1bcb68a643
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-03 19:33:51 +0500

    edit app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,7 @@ window.Alpine = Alpine
 window.flatpickr = flatpickr
 
 import("./../../node_modules/flatpickr/dist/flatpickr.min.css");
-import("./../../vendor/power-components/livewire-powergrid/dist/powergrid.css");
-import("./../../vendor/power-components/livewire-powergrid/dist/tom-select.css");
-
-import TomSelect from "tom-select";
-window.TomSelect = TomSelect
-
+import("./../../vendor/power-components/livewire-powergrid/dist/powergrid.css")
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 
 Alpine.start()
