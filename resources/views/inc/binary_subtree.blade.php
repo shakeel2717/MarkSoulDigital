e5f68a300fcbc11bf8b9321e778f54bec6c2cb6d
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-11 02:53:25 +0500

    three level securtiy

--- resources/views/inc/binary_subtree.blade.php
+++ resources/views/inc/binary_subtree.blade.php
@@ -1,19 +1,20 @@
-<li>
-    <a href="#"  class="{{ $subuser->status == 'active' ? 'border-primary' : 'border-dark' }}" data-bs-toggle="modal" data-bs-target="#TreeDetail{{$subuser->id}}">
+@if ($level < 4) <li>
+    <a href="#" class="{{ $subuser->status == 'active' ? 'border-primary' : 'border-dark' }}" data-bs-toggle="modal" data-bs-target="#TreeDetail{{$subuser->id}}">
         <img src="{{ asset('assets/images/users/user-dummy-img.jpg') }}" alt="Tree user" width="80">
         <h5 class="mb-0">{{ $subuser->name }}</h5>
     </a>
     @include('inc.tree-detail', ['user' => $subuser->id])
     <ul>
         @if ($subuser->left_user)
-        @include('inc.binary_subtree', ['subuser' => $subuser->left_user])
+        @include('inc.binary_subtree', ['subuser' => $subuser->left_user,'level' => $level + 1])
         @else
         @include('inc.binary_subtree_empty')
         @endif
         @if ($subuser->right_user)
-        @include('inc.binary_subtree', ['subuser' => $subuser->right_user])
+        @include('inc.binary_subtree', ['subuser' => $subuser->right_user,'level' => $level + 1])
         @else
         @include('inc.binary_subtree_empty')
         @endif
     </ul>
-</li>
+    </li>
+    @endif
