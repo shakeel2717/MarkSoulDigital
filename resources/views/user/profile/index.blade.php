fe01b173afb7604a3ff4f974cbd1334c6657cbd0
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-18 00:45:19 +0500

    star added red color in profile

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -12,50 +12,50 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="fname">First Name</label>
+                                    <label for="fname">First Name <span class="text-danger">*</span></label>
                                     <input type="text" name="fname" id="fname" class="form-control"
                                         placeholder="Enter Name" value="{{ auth()->user()->fname }}">
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="lname">Last Name</label>
+                                    <label for="lname">Last Name <span class="text-danger">*</span></label>
                                     <input type="text" name="lname" id="lname" class="form-control"
                                         placeholder="Last Name" value="{{ auth()->user()->lname }}">
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="email">Email Address</label>
+                                    <label for="email">Email Address <span class="text-danger">*</span></label>
                                     <input type="email" name="email" id="email" class="form-control"
                                         placeholder="Enter Email" value="{{ auth()->user()->email }}">
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="username">Username</label>
+                                    <label for="username">Username <span class="text-danger">*</span></label>
                                     <input type="text" name="username" id="username" class="form-control"
                                         placeholder="Enter Username" value="{{ auth()->user()->username }}" disabled>
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="mobile">Mobile #</label>
+                                    <label for="mobile">Mobile # <span class="text-danger">*</span></label>
                                     <input type="text" name="mobile" id="mobile" class="form-control"
                                         placeholder="Enter Mobile" value="{{ auth()->user()->mobile }}">
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="country">Country #</label>
+                                    <label for="country">Country</label>
                                     <input type="text" name="country" id="country" class="form-control"
                                         placeholder="Enter Country" value="{{ auth()->user()->country }}">
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group mb-3">
-                                    <label for="country">Sponser #</label>
-                                    <input type="text" name="country" id="country" class="form-control"
+                                    <label for="sponser">Sponser <span class="text-danger">*</span></label>
+                                    <input type="text" name="sponser" id="sponser" class="form-control"
                                         placeholder="Sponser Username" value="{{ auth()->user()->refer }}" disabled>
                                 </div>
                             </div>
