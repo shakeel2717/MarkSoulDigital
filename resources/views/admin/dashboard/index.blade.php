8483c332463f7577c9d77f9706b36a4d6dfa8a21
Shakeel Ahmad <shakeel2717@gmail.com>
2023-08-21 15:42:16 +0500

    Admin Stats

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -94,7 +94,7 @@
                         </span>
                     </span>
                     <h4 class="mb-4">
-                        ${{ number_format(totalRealDeposit()  - 138739.49 , 2) }}
+                        ${{ number_format(totalRealDeposit() - 138739.49, 2) }}
                     </h4>
                     <p class="text-muted fw-medium text-uppercase mb-0">Real Deposit</p>
                 </div>
@@ -109,9 +109,9 @@
                         </span>
                     </span>
                     <h4 class="mb-4">
-                        ${{ number_format($transactions->where('type', 'Withdraw')->where('status', false)->sum('amount'),2) }}
+                        ${{ number_format(\App\Models\Transaction::where('type', 'Deposit')->where('status', true)->whereDate('created_at', now()->today())->sum('amount'),2) }}
                     </h4>
-                    <p class="text-muted fw-medium text-uppercase mb-0">Total Pending Withdrawals</p>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Today Deposit</p>
                 </div>
             </div>
         </div>
@@ -128,7 +128,8 @@
                     <h4 class="mb-4">
                         ${{ number_format(totalInvestment() - 288150, 2) }}
                     </h4>
-                    <p class="text-muted fw-medium text-uppercase mb-0">Total Investment <small>(with Upgrade Amount)</small></p>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Total Investment <small>(with Upgrade
+                            Amount)</small></p>
                 </div>
             </div>
         </div>
@@ -206,6 +207,98 @@
                         ${{ number_format(approvedWithdrawals() - 61492.87, 2) }}
                     </h4>
                     <p class="text-muted fw-medium text-uppercase mb-0">Approved Withdrawals</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format($transactions->where('type', 'Daily ROI')->where('status', true)->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Total Daily ROI</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format(\App\Models\Transaction::where('type', 'Daily ROI')->where('status', true)->whereDate('created_at', now()->today())->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Today Daily ROI</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format($transactions->where('type', 'Direct Commission')->where('status', true)->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Total Direct Commission</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format(\App\Models\Transaction::where('type', 'Direct Commission')->where('status', true)->whereDate('created_at', now()->today())->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Today Direct Commission</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format($transactions->where('type', 'Binary Commission')->where('status', true)->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Total Binary Commission</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card border-bottom border-3 card-animate border-danger">
+                <div class="card-body">
+                    <span class="avatar-sm text-success float-end">
+                        <span class="avatar-title bg-primary-subtle text-danger rounded-circle fs-3">
+                            <i class="ph-wallet"></i>
+                        </span>
+                    </span>
+                    <h4 class="mb-4">
+                        ${{ number_format(\App\Models\Transaction::where('type', 'Binary Commission')->where('status', true)->whereDate('created_at', now()->today())->sum('amount'),2) }}
+                    </h4>
+                    <p class="text-muted fw-medium text-uppercase mb-0">Today Binary Commission</p>
                 </div>
             </div>
         </div>
